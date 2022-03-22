5a86553917e31be6983bedf40dfe928e5169b033
Gabi <ghinea.gabriel96@yahoo.com>
2022-03-22 13:01:44 +0200

    Added possible structure

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from scipy import stats
+
+import datetime
+import os
+import re
+
+if __name__ == "__main__":
+    print('okay!')
